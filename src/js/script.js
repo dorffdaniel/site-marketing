70bff28ef4df064f4ede64a5e41c7bddc2540c34
Dorffdaniel <danielcontabill@hotmail.com>
2025-06-02 20:11:24 +0100

    react e bootstrap add

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+const raiz = document.getElementById('root');
+
+const p = React.createElement('p', null, 'Transformamos ideias em estratégias digitais inovadoras para impulsionar seu negócio, conectar sua marca ao público certo e gerar resultados reais no mundo online.')
+
+const root = ReactDOM.createRoot(raiz)
+root.render(p);
